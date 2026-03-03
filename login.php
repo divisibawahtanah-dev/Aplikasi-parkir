721694cdb28ae2253b21c57d1628a8f7a3bbb460
divisibawahtanah-dev <divisibawahtanah@gmail.com>
2026-03-04 05:51:17 +0700

    fix: bulk update all paths to use __DIR__ for Vercel compatibility

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'config/database.php';
-require_once 'functions/helpers.php';
+require_once __DIR__ . '/config/database.php';
+require_once __DIR__ . '/functions/helpers.php';
 
 if (isset($_SESSION['user'])) {
     redirect('index.php');
